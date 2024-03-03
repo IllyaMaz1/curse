af86b32b867245249799e6c820046237173f1dd6
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-04 01:58:02 +0200

    task3: update setter

--- src/main/java/fifthModule/SpaceShip.java
+++ src/main/java/fifthModule/SpaceShip.java
@@ -3,19 +3,29 @@ package fifthModule;
 public class SpaceShip {
     private String name;
 
-    //Test output
-    public static void main(String[] args) {
-        SpaceShip ship = new SpaceShip();
-        System.out.println(ship.getName()); //Should be null
-        ship.setName("Walker");
-        System.out.println(ship.getName()); //Should be Walker
-    }
-
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (name.length() < 100 && (!name.isBlank() || !name.equals(""))) {
+            this.name = name;
+        }
+        else {
+            getName();
+        }
+    }
+
+    //Test output
+    public static void main(String[] args) {
+        SpaceShip ship = new SpaceShip();
+        ship.setName("Walker");
+        System.out.println(ship.getName()); //Should be Walker
+
+        ship.setName("");
+        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+
+        ship.setName("Voyager ".repeat(100));
+        System.out.println(ship.getName()); //Should be Walker, too long value ignored
     }
 }
