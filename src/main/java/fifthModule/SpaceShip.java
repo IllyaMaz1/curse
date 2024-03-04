e35c3a419a4822cfe9bf19b830cfc7155be0c311
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-04 02:07:24 +0200

    task4: added private variable and getter/setter for it.

--- src/main/java/fifthModule/SpaceShip.java
+++ src/main/java/fifthModule/SpaceShip.java
@@ -2,33 +2,40 @@ package fifthModule;
 
 public class SpaceShip {
     private String name;
+    private String serialNumber;
 
     public String getName() {
         return name;
     }
 
     public void setName(String name) {
-        if (name.length() < 100 && (!name.isBlank() || !name.equals(""))) {
-            this.name = name;
+        if (name.isBlank() || name.length() > 100) {
+            return;
         }
-        else {
-            getName();
+
+        this.name = name;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        char[] arr = serialNumber.toCharArray();
+        if (arr[0] == 'S' && arr[1] == 'N' && arr.length == 8) {
+            this.serialNumber = serialNumber;
         }
     }
 
     //Test output
     public static void main(String[] args) {
         SpaceShip ship = new SpaceShip();
-        ship.setName("Walker");
-        System.out.println(ship.getName()); //Should be Walker
 
-        ship.setName("");
-        System.out.println(ship.getName()); //Should be Walker, empty value ignored
+        ship.setSerialNumber("SN506788");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788
 
-        ship.setName("Voyager ".repeat(100));
-        System.out.println(ship.getName()); //Should be Walker, too long value ignored
+        ship.setSerialNumber("EE123456");
+        System.out.println(ship.getSerialNumber()); //Should be SN506788 - old value
     }
 
-    /////Alex
-    
 }
