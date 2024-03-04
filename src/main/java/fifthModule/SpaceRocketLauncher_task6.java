19b8f92b8409408be83663c3647b889a4ecb829b
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-04 21:43:16 +0200

    task6: added SpaceRocketLauncher_task6,added two private variables, added getters/setters for them, modified setters, added launchBigRocket, launchSmallRocket, getTotalPower

---
+++ src/main/java/fifthModule/SpaceRocketLauncher_task6.java
@@ -0,0 +1,47 @@
+package fifthModule;
+
+public class SpaceRocketLauncher_task6 {
+
+    private int bigRocketCount;
+    private int smallRocketCount;
+
+    public int getBigRocketCount() {
+        return bigRocketCount;
+    }
+
+    public void setBigRocketCount(int bigRocketCount) {
+        if (bigRocketCount < 0 || bigRocketCount > 100) {
+          return;
+        }
+        this.bigRocketCount = bigRocketCount;
+    }
+
+    public int getSmallRocketCount() {
+        return smallRocketCount;
+    }
+
+    public void setSmallRocketCount(int smallRocketCount) {
+        if (smallRocketCount < 0 || smallRocketCount > 100) {
+            return;
+        }
+        this.smallRocketCount = smallRocketCount;
+    }
+
+    public void launchBigRocket() {
+        if (getBigRocketCount() > 0) {
+            bigRocketCount--;
+            System.out.println("Launch big rocket");
+        }
+    }
+
+    public void launchSmallRocket() {
+        if (getSmallRocketCount() > 0) {
+            smallRocketCount--;
+            System.out.println("Launch small rocket");
+        }
+    }
+
+    public int getTotalPower() {
+       return getBigRocketCount() * 100 + getSmallRocketCount() * 50;
+    }
+}
