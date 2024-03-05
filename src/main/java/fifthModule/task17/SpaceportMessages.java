a9263a0b7502c6b1894675bb205b554a28a540eb
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-06 00:13:31 +0200

    task17: created new package, new class SpaceportMessages and added generateLandingMessage method

---
+++ src/main/java/fifthModule/task17/SpaceportMessages.java
@@ -0,0 +1,8 @@
+package fifthModule.task17;
+
+public class SpaceportMessages {
+    public static String generateLandingMessage(String shipName, String spaceport, String time) {
+        return "Spaceship " + shipName + " landing at " + spaceport + ", time is " + time;
+    }
+
+}
