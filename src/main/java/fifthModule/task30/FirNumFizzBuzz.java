a07b0ca146a3cc75ae2da8d0e26f8493df4d0bc2
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-14 01:09:22 +0200

    task30: resolved exercise.

---
+++ src/main/java/fifthModule/task30/FirNumFizzBuzz.java
@@ -0,0 +1,14 @@
+package fifthModule.task30;
+
+public class FirNumFizzBuzz extends FirNum {
+    @Override
+    public int calc(int n) {
+        int summa = 0;
+        for (int i = 0; i <= n; i++) {
+            if ((i % 3 == 0 || i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0)) {
+                summa += i;
+            }
+        }
+        return summa;
+    }
+}
