086f22d13d9e227ad5069257cb15a61512305052
Ilya13851 <105873156+Ilya13851@users.noreply.github.com>
2024-03-13 18:00:17 +0200

    Update Engine.java

--- src.main.java/task9/Engine.java
+++ src.main.java/task9/Engine.java
@@ -1,4 +1,40 @@
 package task9;
 
-public class Engine {
+class Engine {
+    private int power;
+
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getFuelType() {
+        return "A500";
+    }
+
+    public static void main(String[] args) {
+        AdvancedXFuelEngine engine = new AdvancedXFuelEngine();
+        engine.setPower(1000);
+        engine.setSerialNumber("SN504030");
+        engine.printInfo(); //Serial number is SN504030, power is 1000
+    }
+}
+
+class XFuelEngine extends Engine {
+    protected String serialNumber;
+    
+    public String getFuelType() {
+        return "XFuel";
+    }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
+class AdvancedXFuelEngine extends XFuelEngine {
+    public void printInfo() {
+        System.out.println("Serial number is " + serialNumber + ", power is " + getPower());
+    }   
 }
