91be6b4f94eece0a071928d3321a596a98595385
Ilya13851 <105873156+Ilya13851@users.noreply.github.com>
2024-03-14 09:00:04 +0200

    Task12: expanded the program and wrote two classes of passengers: regular and VIP

---
+++ src.main.java/task12/Passenger.java
@@ -0,0 +1,28 @@
+package task12;
+
+abstract class Passenger {
+    public abstract String getType();
+
+    public abstract int getTicketPrice();
+}
+class RegularPassenger extends Passenger {
+
+    public String getType(){
+        return "Regular";
+    }
+    @Override
+    public int getTicketPrice() {
+        return 199;
+    }
+}
+class VIPPassenger extends Passenger {
+
+    @Override
+    public String getType(){
+        return "VIP";
+    }
+    @Override
+    public int getTicketPrice() {
+        return 399;
+    }
+}
