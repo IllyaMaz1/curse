791b484bb7df6f6a9e948913e0ecbc71cd51e158
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-18 21:10:34 +0200

    task32: resolved exercise.

---
+++ src/main/java/fifthModule/task32/Season.java
@@ -0,0 +1,6 @@
+package fifthModule.task32;
+
+public abstract class Season {
+    public abstract String getName();
+    public abstract boolean isCold();
+}
---
+++ src/main/java/fifthModule/task32/Summer.java
@@ -0,0 +1,13 @@
+package fifthModule.task32;
+
+public class Summer extends Season {
+    @Override
+    public String getName() {
+        return "summer";
+    }
+
+    @Override
+    public boolean isCold() {
+        return false;
+    }
+}
