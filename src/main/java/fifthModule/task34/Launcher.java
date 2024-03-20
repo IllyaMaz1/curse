d2faac320dd3e9cb8273f001f3c952a5c3a18ef4
Alexey Nykytiuk <nikityuk.alexey@gmail.com>
2024-03-20 12:13:05 +0200

    task34: resolved exercise.

---
+++ src/main/java/fifthModule/task34/Launcher.java
@@ -0,0 +1,8 @@
+package fifthModule.task34;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Spaceport spaceport = new Spaceport();
+        spaceport.launch();
+    }
+}
